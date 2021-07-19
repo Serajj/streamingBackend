ea2898b95802957e2f87febb2c3356fe921ea042
Seraj Alam <rajalam2372@gmail.com>
2021-07-19 20:58:44 +0530

    xx

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let nodemailer= require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 487,
+    port: 465,
     secure: true,
     auth: {
         user: "rajalam2372@gmail.com" ,
